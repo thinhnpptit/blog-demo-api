7e7a16ce825ce2a56873f4fb9c9e3c9cbfab8c46
Jimmy Nguyen <59567221+thinhnpptit@users.noreply.github.com>
2021-05-25 23:07:39 +0700

    edit route service provider

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -72,17 +72,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        // Route::prefix('api')
-        //     ->middleware('api')
-        //     ->namespace($this->namespace)
-        //     ->group(base_path('routes/api.php'));
-        Route::group([
-            'middleware' => ['api', 'cors'],
-            'namespace' => $this->namespace,
-            'prefix' => 'api',
-        ], function ($router) {
-             //Add you routes here, for example:
-            Route::apiResource('/posts','PostController');
-        });
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
