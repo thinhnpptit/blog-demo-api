23e6bec6d87554d6ab1d83792e5550181746a141
Jimmy Nguyen <59567221+thinhnpptit@users.noreply.github.com>
2021-05-28 22:59:51 +0700

    fix cors middleware again

--- src/app/Http/Middleware/Cors.php
+++ src/app/Http/Middleware/Cors.php
@@ -17,7 +17,6 @@ class Cors
     {
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
